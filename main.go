4eb152c464d5f6bb484f655f38574c6fdef293d7
sd-noob <sdhriday77@gmail.com>
2021-02-14 21:14:26 +0600

    switch_case

--- main.go
+++ main.go
@@ -5,18 +5,20 @@ import (
 )
 
 func main(){
+	
 	fmt.Print("Enter your age:")
 	var age int
 	fmt.Scanf("%d",& age)
 	//fmt.Println( age)
 	
-	if age < 3 {
+	switch age {
+	case 2:
  		fmt.Println("Infant")
-	}else if age > 2 && age < 12 {
+	case 3,4,5,6,7,8,9,10,11,12:
 		fmt.Println("Children")
-	}else if age >= 12 && age <= 19 {
+	case 13,14,15,16,17,18,19:
 		fmt.Println("Teen age")
-	}else{
+	default:
 		fmt.Println("Adult")
 	}
 	
